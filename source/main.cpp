e50c72c8a41cb3de1ae4fe63db747c583e9635a4
emir4169 <89991965+emir4169@users.noreply.github.com>
2022-10-24 16:15:35 +0300

    whyy

--- source/main.cpp
+++ source/main.cpp
@@ -8,11 +8,12 @@ u16 Pressed;
 u16 Held;
 u16 Released;
 int main(void) {
- while(1) {Pressed = keysDown();Held = keysHeld();Released = keysUp();};
- while (Pressed = BIT(0)) {sprintf(Text2, "%s", MyName2);iprintf(Text2);};
+lcdMainOnBottom();
 consoleDemoInit();
 sprintf(Text, "%s", MyName);
 iprintf(Text);
+while(1){Pressed = keysDown();Held = keysHeld();Released = keysUp();};
+while(1){while(Pressed = BIT(0)) {sprintf(Text2, "%s", MyName2);iprintf(Text2);};};
 powerOn(POWER_ALL_2D);
 while(1) {
 swiWaitForVBlank();
