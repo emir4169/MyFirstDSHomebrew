8ecac787a62adc1ac6f855b3778edd3a6d75acca
emir4169 <89991965+emir4169@users.noreply.github.com>
2022-10-24 15:58:50 +0300

    i spent too long on this
    why

--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,15 @@
 #include <nds.h>
 #include <stdio.h>
 char Text[100];
+char Text2[100];
 char MyName[100]="AAAAAAAAA";
+char MyName2[100] ="HAI";
+u16 Pressed;
+u16 Held;
+u16 Released;
 int main(void) {
- 
+ while(1) {Pressed = keysDown();Held = keysHeld();Released = keysUp();};
+ while (Pressed = BIT(0)) {sprintf(Text2, "%s", MyName2);iprintf(Text2);};
 consoleDemoInit();
 sprintf(Text, "%s", MyName);
 iprintf(Text);
