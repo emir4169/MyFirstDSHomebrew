b04e2d71df17d199ed789d6da974a49890158585
emir4169 <89991965+emir4169@users.noreply.github.com>
2022-10-24 16:51:37 +0300

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -8,20 +8,21 @@ u16 Pressed;
 u16 Held;
 u16 Released;
 int main(void) {
-scanKeys();
 lcdMainOnBottom();
 consoleDemoInit();
 sprintf(Text, "%s", RealText);
 iprintf(Text);
 powerOn(POWER_ALL_2D);
 while(1) {
+    if (Pressed & KEY_B) break;
+    scanKeys();
     /* Scan Input. */
     Pressed  = keysDown();
     Held     = keysHeld();
     Released = keysUp();
 
     /* Check pressed state. */
-    if (Pressed & KEY_A) { // Or if (Pressed & KEY_A) for a press.
+    if (Pressed) { // Or if (Pressed & KEY_A) for a press.
         iprintf("%s", RealText2);
     }
 
